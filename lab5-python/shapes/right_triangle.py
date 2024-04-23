ce3b6dae69f0aa3e69736fe8f4caa39a73327d02
michigan-musicer <musicer@umich.edu>
2024-04-23 18:22:58 +0200

    add challenge

---
+++ lab5-python/shapes/right_triangle.py
@@ -0,0 +1,7 @@
+# CHALLENGE: right triangle should inherit from Triangle and have the same functionality
+# except that the area calculation is different: use base * height / 2, and
+# __init__() should take a base and height instead of s1, s2, s3.
+# All functions in RightTriangle should run correctly, including get_perimeter()!
+
+def RightTriangle():
+    # ...
