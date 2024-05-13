af5b70209f82eff93d9e13862e561bfcb69ac487
michigan-musicer <musicer@umich.edu>
2024-05-13 12:03:42 +0200

    add lab7 python

---
+++ lab7-python/README.md
@@ -0,0 +1,5 @@
+For additional English-language reading and videos on binary search trees, see below.
+
+- Higher-level conceptual review: https://courses.engr.illinois.edu/cs225/fa2019/resources/bst/
+- Implementational details (jump to section 18.6): https://ajzhou.gitlab.io/eecs281/notes/chapter18/
+- Video series: https://www.youtube.com/watch?v=JfSdGQdAzq8&list=PLDV1Zeh2NRsB6SWUrDFW2RmDotAfPbeHu&index=24&ab_channel=WilliamFiset
---
+++ lab7-python/lottery_numbers.py
@@ -0,0 +1,31 @@
+from bst import BST
+
+class LotteryTicket:
+    def __init__(self, name : str, nums : list) -> None:
+        self.name = name
+        self.nums = nums
+
+class Lottery:
+    # Construct lottery numbers from an input list of 5 numbers.
+    def __init__(self, nums) -> None:
+        if len(nums) != 5:
+            raise ValueError("nums should have exactly 5 numbers")
+        self._nums = BST()
+        for num in nums:
+            self._nums.insert(num)
+
+    # Determine if the ticket is winning. A ticket wins if all 5 numbers on the ticket
+    # match the 5 lottery numbers.
+    # If it is winning, print "Congratuations, <owner name>, you win!"
+    # Else, print "Sorry, <owner name>, no win today."
+    def check_ticket(self, ticket : LotteryTicket) -> None:
+        # REPLACE pass WITH YOUR IMPLEMENTATION
+        pass
+
+if __name__ == "__main__":
+    bobs_ticket = LotteryTicket("Bob", [1, 2, 3, 4, 5])
+    susans_ticket = LotteryTicket("Susan", [48, 9, 22, 30, 11])
+
+    lottery = Lottery([11, 22, 9, 48, 30])
+    lottery.check_ticket(bobs_ticket)
+    lottery.check_ticket(susans_ticket)
